8dd8a35e480a962d5cce30d1269e10e8e78514a1
Leah Vance <leahvance6312@gmail.com>
2024-07-29 11:27:39 -0500

    ditched Import-Excel

--- create_entry.ps1
+++ create_entry.ps1
@@ -1,9 +1,5 @@
 using module .\inventory.psm1
 
-if(-not (Get-Command Import-Excel)){
-    Install-Module -Name ImportExcel -Force
-}
-
 $sites = ("&COMO", "&CAMO", "&COPRO", "&MOBO")
 
 $computerInfo = [ComputerInfo]::new()
@@ -15,44 +11,53 @@ if($computerInfo.inUse){
     $computerInfo.owner = Read-Host -Prompt "Who is the owner/primary user of this machine?"
 }
 
-$sheetName = $sites[$host.UI.PromptForChoice('', "Enter the location code to create an entry for", $sites, 0)].Replace("&", "") + "_Inventory.xlsx"
+$sheetName = $sites[$host.UI.PromptForChoice('', "Enter the location code to create an entry for", $sites, 0)].Replace("&", "") + "_Inventory.csv"
 $sheetPath = "$PSScriptRoot\$sheetName"
 
 if(-not [System.IO.File]::Exists($sheetPath)){
     New-Item -Name $sheetName -Path $PSScriptRoot -ItemType File
 }
 
-$data = Import-Excel $sheetPath
+$data = Import-Csv -Path $sheetPath
 $duplicateEntry = ($false, 0)
 
-$i = 0
-foreach($item in $data){
-    if($item.Serial -eq $computerInfo.serial){
-        $duplicateEntry = ($true, $i)
-    }
-    $i++
-}
-
-if($duplicateEntry[0]){
-    switch($host.UI.PromptForChoice('', "Entry with serial `"$($computerInfo.serial)`" already exists. Replace entry?", ("&No", "&Yes"), 0)){
-        0 {
-            Write-Host "No entry created. Exiting script." -ForegroundColor Red
+if($data -and ($data.GetType()).Name -eq "Object[]"){
+    $i = 0
+    foreach($item in $data){
+        if($item.Serial -eq $computerInfo.serial){
+            $duplicateEntry = ($true, $i)
         }
-        
-        1 {
-            if($data.Count -lt 2) {
-                $data = $computerInfo.GenerateExcelRow()
-            } else {
+        $i++
+    }
+
+    if($duplicateEntry[0]){
+        switch($host.UI.PromptForChoice('', "Entry with serial `"$($computerInfo.serial)`" already exists. Replace entry?", ("&No", "&Yes"), 0)){
+            0 {
+                Write-Host "No entry created. Exiting script." -ForegroundColor Red
+            }
+            
+            1 {
                 $data[$duplicateEntry[1]] = $computerInfo.GenerateExcelRow()
             }
         }
+    } else {
+        $data += $computerInfo.GenerateExcelRow()
     }
-} elseif($data.Count -lt 1) {
-    $data = $computerInfo.GenerateExcelRow()
-} elseif(($data.GetType().Name) -eq "PSCustomObject"){
-    $data = @($data, $computerInfo.GenerateExcelRow())
-} else {
-    $data += $computerInfo.GenerateExcelRow()
+    
+    
+
+} 
+
+elseif($data -and ($data.GetType()).Name -eq "PSCustomObject"){
+    $data = @($data, $computerInfo.GenerateExcelRow())   
 }
 
-$data | Export-Excel $sheetPath
+else {
+    $data = $computerInfo.GenerateExcelRow()
+}
+
+
+
+
+Remove-Item -Path $sheetPath -Force
+$data | Select-Object -Property * | Export-Csv -path $sheetPath
